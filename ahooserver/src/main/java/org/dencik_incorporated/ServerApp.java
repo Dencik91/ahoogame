94232e76d4a0cedbbbd9966d3a52980167e22e3d
Dencik <agafonov.denis11@gmail.com>
2024-06-03 12:10:19 +0300

    player socket hashmap

--- ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
+++ ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
@@ -4,29 +4,25 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerApp {
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ServerSocket serverSocket = new ServerSocket(10001);
+        List<Player> players = new ArrayList<>();
 
-        Map<String, Object> playerSocketMap = new HashMap<>();
+        players.add(new Player("valerca", 100));
+        players.add(new Player("iurii", 3));
+        players.add(new Player("jenel", 1));
 
-        playerSocketMap.put("socket", new Socket());
-
-        playerSocketMap.put("socket", new Socket("localhost", 10001));
-        playerSocketMap.put("player", new Player("jimmy", 0));
-
-        System.out.println(playerSocketMap.size());
-        System.out.println(playerSocketMap.get("player"));
-        System.out.println(playerSocketMap.get("socket"));
-
+        players.forEach(ServerApp::printInBrackets);
     }
 
-    public static void printPair(String key, Object value) {
-        System.out.println(key + value);;
+    public static void printInBrackets(Player player) {
+        System.out.println("[" + player + "]");
     }
 }
