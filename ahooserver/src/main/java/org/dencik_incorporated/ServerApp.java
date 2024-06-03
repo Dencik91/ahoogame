6f2d73e6e1ea2d41fc9d523d6778f31d376cbd55
Dencik <agafonov.denis11@gmail.com>
2024-06-03 11:32:31 +0300

    initial commit

--- ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
+++ ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
@@ -7,36 +7,7 @@ import java.net.Socket;
 
 public class ServerApp {
 
-    static Player player1;
-    static Player player2;
-
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        System.out.println("Server started");
-        ServerSocket serverSocket = new ServerSocket(10001);
-        // connection
-        Socket clientSocket = serverSocket.accept();
-        ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
-        Frame frame = (Frame) ois.readObject();
-
-        if (frame.getHeader().equals("connect")) {
-            System.out.println("PLAYER CONNECTED: "
-                    + ((Player)frame.getBody()).getNickName());
-            player1 = (Player)frame.getBody();
-        }
-
-        clientSocket = serverSocket.accept();
-        ois = new ObjectInputStream(clientSocket.getInputStream());
-        frame = (Frame) ois.readObject();
-
-        if (frame.getHeader().equals("connect")) {
-            System.out.println("PLAYER CONNECTED: "
-                    + ((Player)frame.getBody()).getNickName());
-            player2 = (Player)frame.getBody();
-        }
-
-        ois.close();
-        clientSocket.close();
-        serverSocket.close();
 
     }
 }
