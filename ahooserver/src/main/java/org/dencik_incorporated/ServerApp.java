76d0e3d714561939acf12c55f1069a6c776b56ad
Dencik <agafonov.denis11@gmail.com>
2024-06-03 11:49:12 +0300

    player socket hashmap

--- ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
+++ ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
@@ -4,10 +4,25 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServerApp {
 
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerSocket serverSocket = new ServerSocket(10001);
+
+        Map<String, Object> playerSocketMap = new HashMap<>();
+
+        playerSocketMap.put("socket", new Socket());
+
+        playerSocketMap.put("socket", new Socket("localhost", 10001));
+        playerSocketMap.put("player", new Player("jimmy", 0));
+
+        System.out.println(playerSocketMap.size());
+        System.out.println(playerSocketMap.get("player"));
+        System.out.println(playerSocketMap.get("socket"));
 
     }
 }
