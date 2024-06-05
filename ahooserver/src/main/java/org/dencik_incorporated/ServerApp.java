e807ecf3d957c7734f96d446ec6b60d26623724e
Dencik <agafonov.denis11@gmail.com>
2024-06-05 10:14:09 +0300

    Initiate Commit

--- ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
+++ ahooserver/src/main/java/org/dencik_incorporated/ServerApp.java
@@ -7,29 +7,8 @@ import java.net.Socket;
 import java.util.*;
 
 public class ServerApp {
-
-
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Set<Player> players = new HashSet<>();
 
-        players.add(new Player("jon", 10));
-        players.add(new Player("pete", 100));
-        players.add(new Player("jon", 20));
-        players.add(new Player("mat", 100));
-
-        players.forEach(ServerApp::printInBrackets);
-
-        CustomSet<String> experimental = new CustomSet<>();
-        experimental.add("Dencik");
-        experimental.add("Mujic");
-        experimental.add("Serios");
-        experimental.forEach(System.out::println);
-
-
-    }
-
-    public static void printInBrackets(Player player) {
-        System.out.println("[" + player + "]");
     }
 }
 
